80af4acc3068799291bd8119e2c4f12ef4f1507b
Tobias Groß <tobi931@googlemail.com>
2020-10-20 09:33:00 +0200

    0.11.02:- made some simple functions (getters) "inline"

--- include/defaults.h
+++ include/defaults.h
@@ -9,7 +9,7 @@
 #endif
 
 //#error "check version first"
-#define BUILD_VERSION "LED_Control_0.11.01"
+#define BUILD_VERSION "LED_Control_0.11.02"
 
 #ifndef BUILD_VERSION
 #error "We need a SW Version and Build Version!"
